f41fa519eff78970eec961467907735212bc4864
Peter Braun <mail@peter-braun.org>
2013-03-08 13:09:01 +0100

    Initial: Das erste Beispiel.

---
+++ vorlesung1/HelloWorld/gen/de/fhws/mobileapps/vorlesung1/helloworld/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.fhws.mobileapps.vorlesung1.helloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
