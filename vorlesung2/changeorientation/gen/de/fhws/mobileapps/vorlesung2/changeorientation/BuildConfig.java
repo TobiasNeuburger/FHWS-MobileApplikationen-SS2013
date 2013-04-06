7027c2c3ec58836887c11bf729168c10bf9c4540
Peter Braun <mail@peter-braun.org>
2013-04-06 23:28:50 +0200

    Examples shows how to set different layouts dependent on orientation

---
+++ vorlesung2/changeorientation/gen/de/fhws/mobileapps/vorlesung2/changeorientation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.fhws.mobileapps.vorlesung2.changeorientation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
