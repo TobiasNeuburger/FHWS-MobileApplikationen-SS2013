b54589427b69f46de63014d20c929388049a205b
Peter Braun <mail@peter-braun.org>
2013-04-28 14:38:36 +0200

    Servlet to save and load persons

--- vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/ProgressServlet.java
+++ vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/ProgressServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/progress")
 public class ProgressServlet extends HttpServlet 
 {
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
