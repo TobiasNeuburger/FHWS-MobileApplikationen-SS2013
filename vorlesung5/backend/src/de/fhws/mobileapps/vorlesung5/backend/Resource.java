d0689d36d2c1f370eddb9616fca1f01c87b4e560
Peter Braun <mail@peter-braun.org>
2013-04-28 18:20:44 +0200

    RESTful resource for persons

--- vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/Resource.java
+++ vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/Resource.java
@@ -1,16 +1,46 @@
 package de.fhws.mobileapps.vorlesung5.backend;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import java.util.Collection;
 
-@Path("rest")
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+
+@Path("persons")
 public class Resource 
 {
 	@GET
-	@Produces( "text/html" )
-	public String printHelloWorld( )
+	@Produces( "application/json" )
+	public Collection<Person> printHelloWorld( )
 	{
-		return "Hello World";
+		return PersonContainer.getInstance().values();
 	}
+	
+	@GET
+	@Path( "/{personid}" )
+	@Produces( "application/json" )
+	public Person loadPerson( @PathParam( "persionid" ) long personId )
+	{
+		return PersonContainer.getInstance().load(personId);
+	}
+	
+	@POST
+	@Consumes( "application/json" )
+	public Response createPerson( Person person, @Context HttpServletRequest httpRequest )
+	{
+		long id = PersonContainer.getInstance().save(person);
+		String url = httpRequest.getRequestURL().toString();
+		
+		return Response.status(HttpServletResponse.SC_CREATED).header("Location", url + "/" + id ).build();
+	}
+
+	
 }
